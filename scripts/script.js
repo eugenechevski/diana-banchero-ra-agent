6f79f2f63a51c163a8631a0e0c5016b3c641ae21
Yauheni Khvashcheuski <yauhenikh7@gmail.com>
2021-09-19 21:39:55 -0400

    04. Finished styling half of the page

--- scripts/script.js
+++ scripts/script.js
@@ -9,15 +9,70 @@ $(document).ready(function(){
         
         // active link in the navigation bar
 
-        // problem: the color doesn't change when scrolling
-        if (this.scrollY > 750 && this.scrollY < 1250){
-            $('.navbar.sticky .menu li:first-child').addClass("active");
+        // about section
+        if (this.scrollY > 650 && this.scrollY < 1350){
+            $("a[href='#about']").addClass("active");
         }else{
-            $(".navbar.sticky .menu li:first-child").removeClass("active");
+            $("a[href='#about']").removeClass("active");
+        }
+
+        // serices section
+        if (this.scrollY > 1350 && this.scrollY < 1750){
+            $("a[href='#services']").addClass("active");
+        }else{
+            $("a[href='#services']").removeClass("active");
+        }
+
+        // contact section
+        if (this.scrollY > 1750 && this.scrollY < 2250){
+            $("a[href='#contact']").addClass("active");
+        }else{
+            $("a[href='#contact']").removeClass("active");
+        }
+
+        // scroll-up button show/hide script
+        if (this.scrollY > 650){
+            $('.scroll-up').addClass('show');
+        }else{
+            $('.scroll-up').removeClass('show');
         }
     });
 
+    // animate the gallery section with changing the background image
+    
+    var count = 1;
+    var url = "";
+    
+    const phrases = [["Embrace", "Your Passion"], ["Build", "Your Future"], ["Follow", "Your Path"]];
+    var text1 = "";
+    var text2 = "";
 
+    setInterval(function(){
 
+        if (count == 3){
+            count = 1;
+        }else{
+            count++;
+        }
+
+        text1 = phrases[count - 1][1];
+        text2 = phrases[count - 1][0];
+
+        console.log(text1);
+        console.log(text2);
+
+        url = '../images/gallery_' + String(count);
+
+        $('.gallery').css('background-image', 'url(' + url + '.jpg' + ')');
+        $('.gallery-content .text-1').html(text1);
+        $('.gallery-content .text-2').html(text2);
+    }, 4000);
+
+    // slide-up script
+    $('.scroll-up').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css('scrollBehaviour', 'auto');
+    });
 
 });
