6eb6fd6dc3f034c2fb274ee6e4c6254f9c906875
Yauheni Khvashcheuski <yauhenikh7@gmail.com>
2021-09-23 14:26:26 -0400

    05. Polishing the page

--- README.md
+++ README.md
@@ -10,21 +10,27 @@ Content:
     The following sections describes the content of the website. The line with the '-' means the piece of the content is must-have, and the '*' means being implemented or will be in the future.
     
     -Navigation bar(navigation menu with links related to each section of the page)
-    -Header(profile image and hello text)
+    -Home section(profile image and hello text)
     -About me section(introduction)
     -My services(description of all the services offered by the agent)
     -Gallery(decorative element)
-    -My team(introduction of the brokerage who the agent works with)
     *Listings(recent or featured listings offered by the brokerage)
     *Testimonials(reviews about the brokerage and the agent)
     -Contact me(social links and email-submission form)
     -Footer(copyright and the author of the website)
 
     ...
-    
+
+Styles:
+    The following section describes all the styling of the webpage. The line with the '-' means the piece of the content is must-have, and the '*' means being implemented or will be in the future.
+
+    -Active color for the navigation var
+    -Scroll button
+    -Navigation bar appears and disappears smoothly when the position is at the top
+
 
     What I've learnt when was working on the project:
-        -...
+        -Wireframes and mockups
 
 Developement process:
 
@@ -32,5 +38,9 @@ Developement process:
     2. Create the sketch of the page; <- we are here
     3. Style the page based on the skectch;
     4. Animate the page;
-    5. Host the website.
+    5. Polish;
+    6. Host the website;
+    7. Get a feedback;
+
+    
 
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,10 @@
+/* 
+    TODO:
+        1. Fix the scroll y-coodridinates;
+        2. Form validation;
+*/
+
+
 $(document).ready(function(){
     $(window).scroll(function(){
         // scroll navbar on scroll script
@@ -47,8 +54,7 @@ $(document).ready(function(){
     var text1 = "";
     var text2 = "";
 
-    setInterval(function(){
-
+    function updateGallery(){
         if (count == 3){
             count = 1;
         }else{
@@ -58,15 +64,15 @@ $(document).ready(function(){
         text1 = phrases[count - 1][1];
         text2 = phrases[count - 1][0];
 
-        console.log(text1);
-        console.log(text2);
-
         url = '../images/gallery_' + String(count);
 
         $('.gallery').css('background-image', 'url(' + url + '.jpg' + ')');
         $('.gallery-content .text-1').html(text1);
         $('.gallery-content .text-2').html(text2);
-    }, 4000);
+    }
+
+    var timeInterval = 4000;
+    var currentInterval = setInterval(updateGallery, timeInterval);
 
     // slide-up script
     $('.scroll-up').click(function(){
@@ -75,4 +81,34 @@ $(document).ready(function(){
         $('html').css('scrollBehaviour', 'auto');
     });
 
+    // gallery pictures arrows script
+    $('.gallery-content .left-arrow').click(function(){
+
+        // update the gallery
+
+        if (count - 1 == 0){
+            count = 2;
+        }else{
+            count -= 2;
+        }
+
+        updateGallery();
+
+        // clear and update the current interval
+        clearInterval(currentInterval);
+        currentInterval = setInterval(updateGallery, timeInterval);
+        
+    });
+
+     $('.gallery-content .right-arrow').click(function(){
+
+        updateGallery();
+
+        // clear and update the current interval
+        clearInterval(currentInterval);
+        currentInterval = setInterval(updateGallery, timeInterval);
+        
+    });
+
+  
 });
